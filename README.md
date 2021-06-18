7847c6453eed50afadc9542cfd6236cf9b1e15e0
Amit Dubey <amitdubey8888@gmail.com>
2021-06-18 14:18:09 +0530

    new changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#Troop Stop
+### Troop Stop
+
+Live URL: https://eamvi25p1a.execute-api.ap-south-1.amazonaws.com/production/
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const sls = require('serverless-http');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -35,7 +36,7 @@ app.set('superSecret', config.serverSecret);
 
 app.use('/api', apiRoutes);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.status(200);
   res.json({
     success: true,
@@ -45,3 +46,4 @@ app.get('/', function(req, res) {
 
 app.listen(port);
 console.log('Backend server is running on http://localhost:3000');
+module.exports.server = sls(app);
