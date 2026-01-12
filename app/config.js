7afe09e6b4cbc925626630489693be075515a16e
Amit Dubey <amitdubey8888@gmail.com>
2026-01-12 20:10:04 +0530

    Security: Refactor secrets to use environment variables

--- app/config.js
+++ app/config.js
@@ -1,11 +1,13 @@
 const AWS = require("aws-sdk");
-AWS.config.update({region: "ap-south-1"});
+AWS.config.update({ region: "ap-south-1" });
 const S3 = new AWS.S3({ endpoint: 'https://s3.ap-south-1.amazonaws.com' });
+
+require('dotenv').config();
 
 module.exports = {
   S3: S3,
-  database: 'mongodb+srv://troopstop:troopstop@troopstop.kqem8.mongodb.net/troopstop',
-  serverSecret: '$2y$12$4A2phQdjCl9fFvcuTHpn3.l7mZTBNagz4HrApfnY1tJYcyHgalifK',
-  tokenExpire: '30d',
-  google_api_key: 'AIzaSyCJnen-AB1eM6aoGKKLSnA1cyDIQknzD5Q',
+  database: process.env.DATABASE,
+  serverSecret: process.env.SERVER_SECRET,
+  tokenExpire: process.env.TOKEN_EXPIRE,
+  google_api_key: process.env.GOOGLE_API_KEY,
 };
